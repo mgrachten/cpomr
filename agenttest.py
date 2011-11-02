af660d050bea677bca35f30d48457afdf5908fae
Maarten Grachten <maarten.grachten@jku.at>
2011-11-02 22:35:48 +0100

    staffline ready for evaluation, appears to have minor glitches

--- agenttest.py
+++ agenttest.py
@@ -10,21 +10,33 @@ def tls(X):
     """total least squares for 2 dimensions
     """
     u,s,v = nu.linalg.svd(X)
-    return -nu.arctan2(-v[1,1],v[0,1])/(2*nu.pi)
+    v = v.T
+    r = ((nu.arctan2(-v[1,1],v[0,1])-nu.pi)%nu.pi)/nu.pi
+    return r
+
+def getError(x,a):
+    return nu.sum(nu.dot(x,nu.array([nu.cos(a*nu.pi),-nu.sin(a*nu.pi)]).T)**2)**.5
 
 class Agent(object):
-    targetAngleDeg = 0
-    minScore = 0
+    targetAngle = 0
+    maxError = 5
+    maxAngleDev = 1 # in degrees
+    
     def __init__(self,xy):
         self.points = nu.array(xy).reshape((1,2))
-        self.angle = self.targetAngleDeg
+        self.mean = xy
+        self.angle = self.targetAngle
+        self.error = 0
         self.score = 0
         self.adopted = True
         self.age = 0
-        self.nadopted = 0
+        #self.nadopted = 0
 
+    #def evaluate(self):
+    #    return self.error, self.angle
+        
     def __str__(self):
-        return 'Agent: angle: {3}; score: {0} age: {1}; points: {2}'.format(self.score,self.age,len(self.points),self.angle)
+        return 'Agent: angle: {3:03f}; error: {0:03f} age: {1}; points: {2}; eval: {4}'.format(self.error,self.age,self.points.shape[0],self.angle,self.evaluate())
 
     def tick(self):
         self.age += 1
@@ -35,48 +47,158 @@ class Agent(object):
         self.adopted = False
         return not self.died()
     
-    def award(self,xy0,xy1=None):
-        #print('award ')
-        self.adopted = True
-        self.nadopted += 1
-        self.points = nu.vstack((self.points,xy0))
-        print('tls',tls(self.points-nu.mean(self.points,0)))
-        self.angle = (2*tls(self.points[:,(1,0)]-nu.mean(self.points[:,(1,0)],0)))%1
-        print('points',self.points)
-        print('angle',self.angle)
+    def died(self):
+        angleOK = nu.abs((self.angle*180-self.targetAngle+90)%180-90) <= self.maxAngleDev
+        errorOK = self.error <= self.maxError
+        successRateOK = self.score >= self.minScore
+        #print('dying?')
+        #print(self)
+        #print('angleOK',angleOK)
+        #print('errorOK',errorOK)
+        #print('successRateOK',successRateOK)
+        return not all((angleOK,errorOK,successRateOK))
+  
+    def getIntersection(self,xy0,xy1):
+        # NB: x and y coordinates are in reverse order
+        # because of image conventions: x = vertical, y = horizontal
         
+        dy,dx = nu.array(xy0-xy1,nu.float)
+        # slope of new line
+        slope = dy/dx
+        ytilde,xtilde = xy0-self.mean
+        # offset of new line
+        b = (ytilde-slope*xtilde)
+
+        # special case 1: parallel lines
+        if slope == nu.tan(nu.pi*self.angle):
+            print('parallel',slope)
+            return None
+
+        # special case 2: vertical line
+        if nu.isinf(slope):
+            # x constant
+            x = xy0[1]
+            y = nu.tan(nu.pi*self.angle)*(x - self.mean[1])+self.mean[0]
+            return nu.array((y,x))
+        # special case 3: line undefined
+        if nu.isnan(slope):
+            # undefined slope, constant y
+            return None
+
+        x = b/(nu.tan(nu.pi*self.angle)-slope)
+        return nu.array((slope*x+b,x))
+    
+    def evaluate(self):
+        # + points
+        # - error
+        # - nu.abs(self.angle - self.targetAngle)
+        # angleDiff = nu.abs((self.angle*180-self.targetAngle+90)%180-90)
+        angleDiff = nu.abs((self.angle-self.targetAngle/180.+.5)%1-.5)
+        v = (.01*self.age+self.points.shape[0]/(1.0+self.age))/(1+self.error+100*angleDiff)
+        return v
+
+    def award(self,xy0,xy1=None):
+        self.adopted = True
+        #self.nadopted += 1
+        if xy1 != None:
+
+            error0 = nu.dot(xy0-self.mean,nu.array([nu.cos(self.angle*nu.pi),-nu.sin(self.angle*nu.pi)]))
+            error1 = nu.dot(xy1-self.mean,nu.array([nu.cos(self.angle*nu.pi),-nu.sin(self.angle*nu.pi)]))
+            if nu.sign(error0) != nu.sign(error1):
+                xy = self.getIntersection(xy0,xy1)
+            else:
+                xy = (xy0,xy1)[nu.argmin(nu.abs([error0,error1]))]
+            
+        else:
+            xy = xy0
+        #print(xy0,xy1,xy,self.mean)
+        #print('awarded!0')
+        #print(self.points)
+        self.points = nu.vstack((self.points,xy))
+        #print('awarded!1')
+        #print(self.points)
+        self.mean = nu.mean(self.points,0)
+        self.angle = tls(self.points-self.mean)
+        self.error = getError(self.points-self.mean,self.angle)
+        #print('mae',self.mean,self.angle,self.error)
+
     def bid(self,xy0,xy1=None):
-        #angle = (nu.arctan2(*(xy-nu.mean(self.points,0)))%nu.pi)/nu.pi
-        a0 = nu.arctan2(*(xy0-nu.mean(self.points,0)))/nu.pi
-        e0 = self.evaluateAngle(a0)
-        #print('points',self.points)
-        #print('point',xy0)
-        #print('ae',a0,e0)
-        return e0
+        #print('agent {0} bidding:'.format(self))
+        #print(' mean: {0}:'.format(self.mean))
 
-    def evaluateAngle(self,a):
-        ad = nu.array([a,(a+1)%1])-self.angle
-        return ad[nu.argmin(nu.abs(ad))]
-        #return ((a-self.angle)+1)%1
+        # distance of xy0 to the current line (defined by self.angle)
+        error0 = nu.dot(xy0-self.mean,nu.array([nu.cos(self.angle*nu.pi),-nu.sin(self.angle*nu.pi)]))
+        #print('\tp0 error',xy0,error0)
+        if xy1 == None:
+            return nu.abs(error0)
+        error1 = nu.dot(xy1-self.mean,nu.array([nu.cos(self.angle*nu.pi),-nu.sin(self.angle*nu.pi)]))
+        #print('\tp1 error',xy1,error1)
+        if nu.sign(error0) != nu.sign(error1):
+            return 0.0
+        else:
+            return min(nu.abs(error0),nu.abs(error1))
+        
+    def getRotMatrix(self):
+        return nu.array([[nu.sin(self.angle*nu.pi),nu.cos(self.angle*nu.pi)],
+                         [nu.cos(self.angle*nu.pi),-nu.sin(self.angle*nu.pi)]])
 
- 
-     
-                
+    def getTrajectory(self):
+        N = self.points.shape[0]
+        if N == 1:
+            return self.points[0,:]
+        #points = nu.round(nu.array([x[0] if len(x) == 1 else nu.mean(nu.array(x),0) 
+        #          for x in self.points])).astype(nu.int)
+        points = self.points
+        tr = []
+        i = 0
+        while i < N-1:
+            dx = points[i+1,1]-points[i,1]
+            dy = points[i+1,0]-points[i,0]
+            delta = max(dx,dy)+2
+            z = nu.round(nu.column_stack((nu.linspace(points[i,0],points[i+1,0],delta),
+                                          nu.linspace(points[i,1],points[i+1,1],delta)))).astype(nu.int).reshape((-1,2))
+            tr.append(z)
+            i += 1
+        return nu.vstack(tr)
+
+
 class BarLineAgent(Agent):
-    targetAngleDeg = 90
-    minScore = -10
+    targetAngle = 90
+    maxError = 5
     
 class StaffLineAgent(Agent):
-    targetAngleDeg = 0
-    minScore = -5
+    targetAngle = 0 # in degrees
+    maxError = 5
+    maxAngleDev = 1 # in degrees
+    minScore = -1
 
 if __name__ == '__main__':
-    x1 = nu.array([0,0])
-    x2 = nu.array([1,100])
-    #x1 = nu.array([0,0])
+    x1 = nu.array([255,29])
+    x2a = nu.array([253,30])
+    x2b = nu.array([256,30])
+    #x3 = nu.array([-.1,-100])
+    x3 = nu.array([5,-1])
+    x3b = nu.array([-5,-1])
+    #xy = nu.vstack((x1,x2))
+
     a = StaffLineAgent(x1)
-    maxAngle = 1/180.
-    b = a.bid(x2)
-    print(b,maxAngle)
-    print(nu.abs(b)<maxAngle)
-    a.award(x2)
+    b = a.bid(x2a,x2b)
+    print('bid result',b)
+    a.award(x2a,x2b)
+    print('tick',a.tick())
+    print(a)
+    sys.exit()
+    print('')
+    b = a.bid(x3,x3b)
+    print('bid',b)
+    print('a before adding')
+    print(a)
+    a.award(x3,x3b)
+    print('tick',a.tick())
+    print(a.points)
+    #a.angle = .499999999
+    #a.mean = nu.array([0,0])
+    #d1 = nu.array([10,1])
+    #d2 = nu.array([-10,1])
+    #print('d',a.getIntersection(d1,d2))
+    
--- barfind.py
+++ barfind.py
@@ -7,16 +7,12 @@ from imageUtil import getImageData, writeImageData, makeMask, normalize, jitterI
 from utilities import argpartition, partition, makeColors
 from main import convolve
 
-def tls(X):
-    """total least squares for 2 dimensions
-    """
-    u,s,v = nu.linalg.svd(X)
-    return nu.arctan2(v[0,1],v[1,1])/(2*nu.pi)
+from agenttest import Agent
 
 class AgentPainter(object):
     def __init__(self,img):
         self.img = nu.array((255-img,255-img,255-img))
-        self.maxAgents = 50
+        self.maxAgents = 100
         self.colors = makeColors(self.maxAgents)
         self.paintSlots = nu.zeros(self.maxAgents,nu.bool)
         self.agents = {}
@@ -49,6 +45,7 @@ class AgentPainter(object):
             c = self.colors[self.agents[agent]]
             c1 = nu.maximum(0,c-50)
             #self.paintStart(agent.point,c)
+            #print(agent.getTrajectory())
             for p in agent.getTrajectory():
                 self.paint(p,c)
             for p in agent.points:
@@ -63,209 +60,7 @@ class AgentPainter(object):
         #print('point',coord,img.shape)
         self.img[:,int(coord[0]),int(coord[1])] = color
 
-class Agent(object):
-    targetAngleDeg = 0
-    minScore = 0
-    def __init__(self,xy):
-        self.points = nu.array(xy).reshape((1,2))
-        self.angle = self.targetAngleDeg
-        self.score = 0
-        self.adopted = True
-        self.age = 0
-        self.nadopted = 0
-    def __str__old(self):
-        return 'Agent: point: {0}; angle: {1}; score: {2} age: {3}; points: {4}'.format(self.point,self.angle,self.score,self.age)
-    def __str__(self):
-        return 'Agent: angle: {3}; score: {0} age: {1}; points: {2}'.format(self.score,self.age,len(self.points),self.angle)
-    def tick(self):
-        self.age += 1
-        if self.adopted:
-            self.score += 1
-        else:
-            self.score -= 1
-        self.adopted = False
-        return not self.died()
-    
-    def award(self,xy0,xy1=None):
-        #print('award ')
-        #print('points',self.points)
-        #print('point',xy0)
-        self.adopted = True
-        self.nadopted += 1
-        self.points = nu.vstack((self.points,xy0))
-        self.angle = (2*tls(self.points[:,(1,0)]-nu.mean(self.points[:,(1,0)])))%1
-        
-    def bid(self,xy0,xy1=None):
-        #angle = (nu.arctan2(*(xy-nu.mean(self.points,0)))%nu.pi)/nu.pi
-        a0 = nu.arctan2(*(xy0-nu.mean(self.points,0)))/nu.pi
-        e0 = self.evaluateAngle(a0)
-        #print('points',self.points)
-        #print('point',xy0)
-        #print('ae',a0,e0)
-        return e0
 
-    def evaluateAngle(self,a):
-        ad = nu.array([a,a%1])-self.angle
-        return ad[nu.argmin(nu.abs(ad))]
-
- 
-    def bidOld(self,xy0,xy1=None):
-        a0 = self.getAngle(xy0)
-        e0 = self.evaluateAngle(a0)
-        if xy1 is None:
-            return e0
-        a1 = self.getAngle(xy1)
-        e1 = self.evaluateAngle(a1)
-        if a0 <= a1:
-            if e0 <= 0 and e1 >= 0:
-                return 0
-            else:
-                if nu.abs(e0) < nu.abs(e1):
-                    return e0
-                else:
-                    return e1
-        else:
-            if e1 <= 0 and e0 >= 0:
-                return 0
-            else:
-                if nu.abs(e0) < nu.abs(e1):
-                    return e0
-                else:
-                    return e1
-
-    def getScore(self):
-        return score
-
-    def died(self):
-        return self.score < Agent.minScore
-
-    def getAngle(self,xy):
-        return nu.arctan2(*(self.point-xy))%nu.pi/nu.pi
-        
-    def getMeanPoints(self):
-        return self.points
-
-    def getMeanPointsOld(self):
-        mp = nu.zeros((len(self.points),2))
-        for i,p in enumerate(self.points):
-            if len(p) > 1:
-                mp[i,:] = nu.mean(nu.array(p).reshape((-1,2)),0)
-            else:
-                mp[i,:] = p[0]
-        return mp
-
-    def evaluate(self):
-        mp = self.getMeanPoints()
-        N = mp.shape[0]
-        dm = nu.zeros((N,N))
-        print(mp)
-        #angles = []
-        for i in range(N):
-            for j in range(i):
-                a = mp[i,:]
-                b = mp[j,:]
-                th_ab = nu.arctan2(*(b-a))
-                se = 0
-                for k in range(N):
-                    if k != i and k !=j:
-                        c = mp[k,:]
-                        th_bc = nu.arctan2(*(c-b))
-                        se += (nu.sin(th_bc-th_ab)*nu.sum((c-b)**2)**.5)**2
-                dm[i,j] = (se/(N-2))**.5
-                dm[j,i] = (se/(N-2))**.5
-        l = cluster.hierarchy.linkage(spatial.distance.squareform(dm))
-        dm = dm[:,nu.argsort(dm[10,:])]
-        dm = dm[nu.argsort(dm[:,2]),:]
-        nu.savetxt('/tmp/dm.txt',dm)
-        maxDist = 7
-        c = cluster.hierarchy.fcluster(l,maxDist,criterion='distance')
-        idict = argpartition(lambda x: x[0], nu.column_stack((c,nu.arange(len(c)))))
-        kv = idict.items()
-        print(c)
-        kv.sort(key=lambda x: len(x[1]))
-        winner = kv[-1][0]
-        losers = nu.where(c != winner)
-        winners = nu.where(c == winner)
-        print('w',winners)
-        print('l',losers)
-
-    def evaluateNew(self):
-        mp = self.getMeanPoints()
-        N = mp.shape[0]
-        dm = nu.zeros((N,N))
-        print(mp)
-        #angles = []
-        for i in range(N):
-            for j in range(i):
-                a = mp[i,:]
-                b = mp[j,:]
-                th_ab = nu.arctan2(*(b-a))
-                dm[i,j] = th_ab
-
-    def getTrajectory(self):
-        N = len(self.points)
-        if N == 1:
-            return self.points[0,:]
-        #points = nu.round(nu.array([x[0] if len(x) == 1 else nu.mean(nu.array(x),0) 
-        #          for x in self.points])).astype(nu.int)
-        points = self.points
-        tr = []
-        i = 0
-        while i < N-1:
-            dx = points[i+1,1]-points[i,1]
-            dy = points[i+1,0]-points[i,0]
-            delta = max(dx,dy)+2
-            z = nu.round(nu.column_stack((nu.linspace(points[i,0],points[i+1,0],delta),
-                             nu.linspace(points[i,1],points[i+1,1],delta)))).astype(nu.int).reshape((-1,2))
-            tr.append(z)
-            i += 1
-        return nu.vstack(tr)
-
-    def awardOld(self,xy0,xy1=None):
-        self.adopted = True
-        self.nadopted += 1
-        if xy1 != None:
-            self.points.append((xy0,xy1))
-        else:
-            self.points.append((xy0,))
-
-        if True:
-            return True
-
-        bestAngle = self.bid(xy0,xy1)
-        self.angle = ((self.nadopted-1)*self.angle+bestAngle)/self.nadopted
-        if xy1 == None:
-            newpoint = xy0
-        else:
-            #newpoint = (xy0+xy1)/2.
-            if xy0[0] == xy1[0]:
-                newpoint = nu.array((xy0[0],self.point[1]+(xy0[0]-self.point[0])*nu.cos(2*nu.pi*bestAngle)))
-            if xy0[1] == xy1[1]:
-                newpoint = nu.array((self.point[0]+(xy0[1]-self.point[1])*nu.sin(2*nu.pi*bestAngle),xy0[1]))
-                if xy0[0] < xy1[0]:
-                    smallest = xy0
-                    largest = xy1
-                else:
-                    smallest = xy1
-                    largest = xy0
-                if newpoint[0] < smallest[0]:
-                    newpoint = smallest
-                if newpoint[0] > largest[0]:
-                    newpoint = largest
-                #print('np',bestAngle,self.point,xy0,xy1,newpoint)
-            else:
-                print('Error, dont know what to do')
-        self.point = ((self.nadopted-1)*self.point+newpoint)/self.nadopted
-        self.points.append(newpoint)
-     
-                
-class BarLineAgent(Agent):
-    targetAngleDeg = 90
-    minScore = -10
-    
-class StaffLineAgent(Agent):
-    targetAngleDeg = 0
-    minScore = -5
         
 def getCrossings(v,agents,AgentType,ap,vert=None,horz=None):
     mindist = 1
@@ -277,7 +72,7 @@ def getCrossings(v,agents,AgentType,ap,vert=None,horz=None):
     else:
         return agents
     #print(vert)
-    print('protocandidates',candidates)
+    #print('protocandidates',candidates)
     if vert is not None:
         candidates = [[nu.array([vert,horz]) for horz in horzz] for horzz in candidates]
     elif horz is not None:
@@ -287,19 +82,28 @@ def getCrossings(v,agents,AgentType,ap,vert=None,horz=None):
     #print('candidates',len(candidates))
     #print('candidates',candidates)
     #print('agents',len(agents))
+
     unadopted = []
-    maxDeviation = 3/360.
-    for i,c in enumerate(candidates):
-        if len(agents) == 0:
-            unadopted.append(i)
-        else:
-            bids = [nu.abs(a.bid(*c)) for a in agents]
-            bestBidder = nu.argmin(bids)
-            #print('bids',bids)
-            if bids[bestBidder] < maxDeviation:
-                #print('c',bids[bestBidder],maxDeviation)
-                agents[bestBidder].award(*c)
-                #ap.drawAgent(c,agents[bestBidder])
+    bids = None
+    if len(agents) == 0:
+        unadopted.extend(range(len(candidates)))
+    else:
+        print('agents, candidates',len(agents),len(candidates))
+        bids = nu.zeros((len(candidates),len(agents)))
+        for i,c in enumerate(candidates):
+            bids[i,:] = [nu.abs(a.bid(*c)) for a in agents]
+
+        cidx = nu.argsort(nu.min(bids,1))
+        #print(bids)
+        #print(cidx)
+        adopters = set([])
+        for i in cidx:
+            #print(bids[i,:])
+            bestBidder = nu.argmin(bids[i,:])
+            bestBet = bids[i,bestBidder]
+            if bestBet <= agents[bestBidder].maxError and not bestBidder in adopters:
+                agents[bestBidder].award(*candidates[i])
+                adopters.add(bestBidder)
             else:
                 unadopted.append(i)
     for i in unadopted:
@@ -309,27 +113,17 @@ def getCrossings(v,agents,AgentType,ap,vert=None,horz=None):
         #for j in candidates[i]:
             #print('unadopted',j)
     
-    agentsn = []
-    for a in agents:
-        alive = a.tick()
-        print('angle',a.angle,a.points)
-        goodAngle = nu.abs(a.angle-a.targetAngleDeg/360.) < maxDeviation
-        if alive and goodAngle:
-            agentsn.append(a)
-        else:
-            pass #ap.unregister(a)
-    
-    return agentsn
+    return [a for a in agents if a.tick()]
 
-
-#def plotTrajectories(img,col,rows):
-#    for i in rows:
-#        img[i,col] = 255
+class StaffLineAgent(Agent):
+    targetAngle = 0 # in degrees
+    maxError = 5 # mean perpendicular distance of points to line (in pixels)
+    maxAngleDev = 2 # in degrees
+    minScore = -7
 
 def findStaffLines(img,fn):
     N,M = img.shape
-    maxStaffLines = 70
-    lookatProportion = .2
+    lookatProportion = .05
     vsums = nu.sum(img,0)
     #vsums = vsums[nu.nonzero(vsums)[0]]
     colorder = nu.argsort(vsums)
@@ -341,40 +135,32 @@ def findStaffLines(img,fn):
     agents = []
     
     ap = AgentPainter(img)
-    #for c in colorder[:int(lookatProportion*N)]:
-    for c in colorder[:100]:
+
+    for c in colorder[:int(lookatProportion*N)]:
+    #for c in colorder[:100]:
         print('column',c)
         agents = getCrossings(img[:,c],agents,StaffLineAgent,ap,horz=c)
-        print('agents',len(agents))
+        #print('agents',len(agents))
         #agents = agents[:maxStaffLines]
-        agents.sort(key=lambda x: x.age*x.score)
+        #agents.sort(key=lambda x: x.age*x.score)
+        agents.sort(key=lambda x: x.evaluate())
         agents.reverse()
-        for a in agents[:50]:
-            print('{0}'.format(a))
+        #for i,a in enumerate(agents[:50]):
+        #    print('{0} {1}'.format(i,a))
     
-    k = 10
-    print(agents[k])
-    #agents[k].evaluate()
+    k = len(agents)-1
+    #k = 30
+    #print(agents[k])
+    j = 0
+    print('columns processed',lookatProportion*N)
     for a in agents[0:k+1]:
-        ap.register(a)
-        ap.drawAgent(a)
+        if a.points.shape[0] > 1 and a.age > .8*lookatProportion*N:
+        #if a.points.shape[0] > 1 and a.age > 60:
+            print('{0} {1}'.format(j,a))
+            j += 1
+            ap.register(a)
+            ap.drawAgent(a)
     ap.writeImage(fn)
-    sys.exit()
-
-
-    print(N/2)
-    agents = agents[:maxStaffLines]
-    agents.sort(key=lambda x: x.age*x.score)
-    agents.reverse()
-    for a in agents:
-        print('{1} {0}'.format(a,a.point[0]))
-    k = nu.argmin(nu.diff([a.score for a in agents]))
-    print(k+1,'stafflines found')
-    for a in agents[:k+1]:
-        print('{1} {0}'.format(a,a.point[0]))
-    print('nr of agents',len(agents))
-    return agents[:10]
-
 
 if __name__ == '__main__':
     fn = sys.argv[1]
@@ -390,16 +176,7 @@ if __name__ == '__main__':
     bgThreshold = 100
     img[img< bgThreshold] = 0
     #img[img> 0] = 255
-    findStaffLines(img[:410,:],fn)
+    #findStaffLines(img[:1000,:],fn)
+    findStaffLines(img,fn)
     #findStaffLines(img[:100,:],fn)
     #nu.savetxt('/tmp/p.txt',nu.sum(img,1))
-    sys.exit()    
-
-#problematic:
-"""
-chopin:
-7 5 1
-18 1 1
-35 2 7
-
-"""
