072cd42d08ef70a46c32d7ec9dc923362eaaa17c
Maarten Grachten <maarten.grachten@jku.at>
2012-01-25 11:22:44 +0100

    removed unused code

--- OMR/bar.py
+++ OMR/bar.py
@@ -108,10 +108,6 @@ class BarCandidate(object):
         assert self.rotator != None
         return self.rotator.derotate(nu.column_stack((nu.zeros(self.barHCoordsLocal.shape[0]),self.barHCoordsLocal)))
 
-    #@cachedProperty
-    #def vSums(self):
-    #    return nu.sum(self.approximateNeighbourhood.astype(nu.float),0)
-
     @cachedProperty
     def estimatedType(self):
         """
@@ -193,23 +189,14 @@ class BarCandidate(object):
         staff1Top,staff1Bot = self.system.staffs[1].getTopBottomLeft()
         extra = 5
         L = int(nu.ceil(staff1Bot-staff0Top)+2*extra)
-        #print(self.system.n,'systemheight',nu.ceil(staff1Bot-staff0Top))
         comb = nu.zeros(L)
-        #comb[extra:extra+L] = 1.0
-        #print(L)
         comb[nu.round(nu.linspace(staff0Top,staff0Bot-1,5)-staff0Top+extra).astype(nu.int)] += 1
         comb[nu.round(nu.linspace(staff1Top,staff1Bot-1,5)-staff0Top+extra).astype(nu.int)] += 1
         comb = comb[::-1]
         conv = nu.convolve(nu.sum(img,1),comb,'valid')
         offset = nu.argmax(conv)
         vCorrection = offset-img.shape[0]/2.0+comb.shape[0]/2.0
-        #print('offset',vCorrection)
-        if True:
-            return vCorrection
-        f = '-{0:03d}-{1:03d}'.format(self.system.n,self.n)
-        nu.savetxt('/tmp/a'+f+'.txt',conv)
-        nu.savetxt('/tmp/img'+f+'.txt',nu.sum(img,1))
-        nu.savetxt('/tmp/c'+f+'.txt',comb[::-1])
+        return vCorrection
 
     def _getNeighbourhood(self,midCorrection=None):
         # approximate system top and bottom (based on global staff detection)
@@ -235,36 +222,6 @@ class BarCandidate(object):
         cimg = getAntiAliasedImg(self.system.correctedImgSegment,xxr,yyr)
         return cimg
 
-    def write(self):
-        getVCenterOfBarline(self.approximateNeighbourhood,self.kRange,True)
-
-    @cachedProperty
-    def points(self):
-        # deprecated?
-        cimg = self.approximateNeighbourhood
-        if cimg == None:
-            return None, None
-        vhalf = int(cimg.shape[0]/2.0)
-        hhalf = int(cimg.shape[1]/2.0)
-        left,hCenter,right = getHCenterAndWidth(cimg)
-        if left == None:
-            return None, None
-        hCorrection = hCenter - hhalf
-        w = right-left
-        print('w,left,hCenter,right',w,left,hCenter,right,hCorrection,cimg.shape)
-        h0 = max(0,left-hCorrection-w)
-        h1 = left-hCorrection
-        h2 = right-hCorrection
-        h3 = min(cimg.shape[1],right-hCorrection+w)
-        vCorrection = getVCenterOfBarline(cimg[:,h0:h3],self.kRange)-vhalf
-        return nu.array((vCorrection,hCorrection)),nu.array((h0,h1,h2,h3))
-
-    @cachedProperty
-    def getStaffVCoords(self):
-        staffTops = self.getVerticalStaffLinePositions()
-        staffBots = staffTops+self.system.getStaffLineWidth()
-        return self.getVerticalStaffLinePositions
-
     @cachedProperty
     def getVerticalStaffLinePositions(self):
         # approximate system top and bottom (based on global staff detection)
@@ -286,133 +243,6 @@ class BarCandidate(object):
         f0 = krng[nu.argmax([nu.sum(hsums[staff0Rows+k]) for k in krng])]
         f1 = krng[nu.argmax([nu.sum(hsums[staff1Rows+k]) for k in krng])]
         return nu.append(staff0Tops+f0,staff1Tops+f1)
-  
-    @cachedProperty
-    def featureIdx(self):
-        #w = self.agent.getLineWidth()
-        h1,h2 = self.getBarHCoords()
-        w = h2-h1
-        h0 = h1-w
-        h3 = h2+w
-        staffTops = self.getVerticalStaffLinePositions()
-        staffBots = staffTops+self.system.getStaffLineWidth()
-        staffTops = nu.round(staffTops).astype(nu.int)
-        staffBots = nu.round(staffBots).astype(nu.int)
-        staffIdx = nu.array([x for x in 
-                             chain.from_iterable([range(staffTops[i],staffBots[i]) 
-                                                  for i in range(len(staffTops))])])
-        interStaffIdx0 = nu.array([x for x in 
-                                   chain.from_iterable([range(staffBots[i]+1,staffTops[i+1]-1) 
-                                                        for i in range(4)])])
-        interStaffIdx1 = nu.array([x for x in 
-                                   chain.from_iterable([range(staffBots[i]+1,staffTops[i+1]-1) 
-                                                        for i in range(5,9)])])
-        interStaffIdx = nu.append(interStaffIdx0,interStaffIdx1)
-        return {'h0':h0,'h1':h1,'h2':h2,'h3':h3,
-                'staffTops':staffTops,'staffBots':staffBots,
-                'staff':staffIdx,'interStaff':interStaffIdx}
-        
-    def checkInterStaffSymmetry(self,n=None):
-        h0 = self.getFeatureIdx()['h0']
-        h1 = self.getFeatureIdx()['h1']
-        h2 = self.getFeatureIdx()['h2']
-        h3 = self.getFeatureIdx()['h3']
-        interStaff = self.getFeatureIdx()['interStaff']
-        nh = self.neighbourhood
-        if n != None:
-            ap = AgentPainter(nh)
-            ml = nu.array([x for x in product(interStaff,nu.arange(h0,h1-1))])
-            mr = nu.array([x for x in product(interStaff,nu.arange(h2+1,h3))])
-            ap.paintRav(ml,color=(255,0,0),alpha=.4)
-            ap.paintRav(mr,color=(255,0,0),alpha=.4)
-            ap.writeImage(n)
-        return nu.mean(nu.abs(nh.astype(nu.int)[interStaff,h0:h1-1]-nh[interStaff,h2+1:h3]))
-
-    @cachedProperty
-    def checkStaffSymmetry(self):
-        h0 = self.getFeatureIdx()['h0']
-        h1 = self.getFeatureIdx()['h1']
-        h2 = self.getFeatureIdx()['h2']
-        h3 = self.getFeatureIdx()['h3']
-        staff = self.getFeatureIdx()['staff']
-        nh = self.neighbourhood
-        print('nh',nh.shape,h0,h1,h2,h3)
-        #return nu.mean(nh.astype(nu.int)[staff,h0:h1-1]-nh[staff,h2+1:h3])
-        return nu.mean(nh.astype(nu.int)[staff,:h1-1]-nh[staff,h2+1:])
-        
-    def checkStaffLines(self):
-        """Return the max staff line blackness on both sides of the bar,
-        after subtracting the blackness above and below the staff lines.
-        This yields low values for lines that are not on a staff (e.g. the accolade)
-        """
-        h0 = self.getFeatureIdx()['h0']
-        h1 = self.getFeatureIdx()['h1']
-        h2 = self.getFeatureIdx()['h2']
-        h3 = self.getFeatureIdx()['h3']
-        staffTops = self.getFeatureIdx()['staffTops']
-        staffBots = self.getFeatureIdx()['staffBots']
-        staffIdx = self.getFeatureIdx()['staff']
-        w = int(nu.ceil(self.system.getStaffLineWidth()))
-        aboveStaffIdx = nu.array([x for x in 
-                                   chain.from_iterable([range(staffTops[i]-w,staffTops[i]) 
-                                                        for i in range(len(staffTops))])])
-        belowStaffIdx = nu.array([x for x in 
-                                   chain.from_iterable([range(staffBots[i],staffBots[i]+w) 
-                                                        for i in range(len(staffTops))])])
-        interStaffIdx = self.getFeatureIdx()['interStaff']
-        nh = self.neighbourhood
-        staffLeft = nu.mean(nh[staffIdx,h0:h1])
-        staffRight = nu.mean(nh[staffIdx,h2:h3])
-
-        aboveStaffLeft = nu.mean(nh[aboveStaffIdx,h0:h1])
-        belowStaffLeft = nu.mean(nh[belowStaffIdx,h0:h1])
-        staffLeft = nu.mean(nh[staffIdx,h0:h1])
-        aboveStaffRight = nu.mean(nh[aboveStaffIdx,h2:h3])
-        belowStaffRight = nu.mean(nh[belowStaffIdx,h2:h3])
-        staffRight = nu.mean(nh[staffIdx,h2:h3])
-        #print('sl',staffLeft,aboveStaffLeft,belowStaffLeft)
-        #print('sr',staffRight,aboveStaffRight,belowStaffRight)
-        #print('r',max(staffLeft-max(aboveStaffLeft,belowStaffLeft),
-        #              staffRight-max(aboveStaffRight,belowStaffRight)))
-        return max(staffLeft-max(aboveStaffLeft,belowStaffLeft),
-                   staffRight-max(aboveStaffRight,belowStaffRight))
-
-    @cachedProperty
-    def getFeatures(self):
-        sd = int(nu.round(self.system.getStaffLineDistance()))
-        h0 = self.getFeatureIdx()['h0']
-        h1 = self.getFeatureIdx()['h1']
-        h2 = self.getFeatureIdx()['h2']
-        h3 = self.getFeatureIdx()['h3']
-        staffTops = self.getFeatureIdx()['staffTops']
-        staffBots = self.getFeatureIdx()['staffBots']
-        staffIdx = self.getFeatureIdx()['staff']
-        interStaffIdx = self.getFeatureIdx()['interStaff']
-        nh = self.neighbourhood
-        
-        barStaff0 = nu.mean(nh[staffTops[0]:staffBots[4],h1:h2])
-        barStaff1 = nu.mean(nh[staffTops[5]:staffBots[9],h1:h2])
-        interStaff = nu.mean(nh[staffBots[4]:staffTops[5],h1:h2])
-        aboveBar = nu.mean(nh[staffTops[0]-sd:staffTops[0],h1:h2])
-        belowBar = nu.mean(nh[staffBots[9]:staffBots[9]+sd,h1:h2])
-        if nu.isnan(aboveBar):
-            aboveBar = 255.
-        if nu.isnan(belowBar):
-            belowBar = 255.
-        staffLeft = nu.mean(nh[staffIdx,h0:h1])
-        staffRight = nu.mean(nh[staffIdx,h2:h3])
-        interStaffLeft = nu.mean(nh[interStaffIdx,h0:h1])
-        interStaffRight = nu.mean(nh[interStaffIdx,h2:h3])
-        # print('bar upper staff',barStaff0)
-        # print('bar lower staff',barStaff1)
-        # print('above bar',aboveBar)
-        # print('below bar',belowBar)
-        # print('inter staffline',interStaff)
-        # print('mean staffline left from staffs',staffLeft)
-        # print('mean staffline right from staffs',staffRight)
-        # print('mean interstaffline left from staffs',interStaffLeft)
-        # print('mean interstaffline right from staffs',interStaffRight)
-        return (barStaff0,barStaff1,aboveBar,belowBar,staffLeft,staffRight,interStaffLeft,interStaffRight,interStaff)
     
 if __name__ == '__main__':
     pass
