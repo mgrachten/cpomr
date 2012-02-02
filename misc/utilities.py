44824800c3531ab99f0d8de3073c718eb02d362b
Maarten Grachten <maarten.grachten@jku.at>
2012-02-02 12:02:07 +0100

    licensing added in subtree

--- misc/utilities.py
+++ misc/utilities.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+#    Copyright 2012, Maarten Grachten.
+#
+#    This file is part of mg_python_modules.
+#
+#    mg_python_modules is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    mg_python_modules is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with mg_python_modules.  If not, see <http://www.gnu.org/licenses/>.
+
 import re,sys,os
 from numpy import array,size,std
 import numpy as nu
